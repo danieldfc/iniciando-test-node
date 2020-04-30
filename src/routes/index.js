ed712906b07a37d7cf53054db3af4443d5c3023b
Daniel Felizardo <daniel.david772@gmail.com>
2020-04-30 19:25:59 -0300

    config: eslint, prettier and editorconfig

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,8 @@ const { Router } = require('express');
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ ok: true });
-});
+routes.get('/', (req, res) => res.json({ ok: true }));
 
-routes.get('/users', (req, res) => {
-  return res.json({ ok: true });
-});
+routes.get('/users', (req, res) => res.json({ ok: true }));
 
 module.exports = routes;
